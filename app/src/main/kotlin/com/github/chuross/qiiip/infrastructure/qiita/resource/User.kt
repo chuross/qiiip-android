578d8136ed78d65cd29e09ad158a45889e1f534d
chuross <azarasi.mo.mo@gmail.com>
2015-11-01 01:08:36 +0900

    APIのユーザー情報を追加

--- app/src/main/kotlin/com/github/chuross/qiiip/infrastructure/qiita/resource/User.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/infrastructure/qiita/resource/User.kt
@@ -1,5 +1,9 @@
 package com.github.chuross.qiiip.infrastructure.qiita.resource
 
 data class User(
-        var id: String?
+        var id: String?,
+        var name: String?,
+        var description: String?,
+        var profileImageUrl: String?,
+        var websiteUrl: String
 )
