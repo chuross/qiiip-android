13ade6db1f2a5338d4347481e8d664102194eced
chuross <azarasi.mo.mo@gmail.com>
2015-10-31 18:26:45 +0900

    kotlinのセットアップ

--- app/src/androidTest/java/com/github/chuross/chuross/qiiip/ApplicationTest.java
+++ app/src/androidTest/java/com/github/chuross/chuross/qiiip/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.github.chuross.qiiip;
+package com.github.chuross.chuross.qiiip;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/kotlin/com/github/chuross/qiiip/HomeActivity.kt
@@ -0,0 +1,35 @@
+package com.github.chuross.qiiip
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.github.chuross.chuross.qiiip.R
+
+class HomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
