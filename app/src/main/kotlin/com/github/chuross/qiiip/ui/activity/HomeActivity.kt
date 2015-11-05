e738bcd8ab1a142b2b17ab3c2b4a4e88d91d34a7
chuross <azarasi.mo.mo@gmail.com>
2015-11-06 03:28:04 +0900

    ホーム画面のViewを配置した

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/HomeActivity.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/HomeActivity.kt
@@ -7,6 +7,7 @@ import com.github.chuross.qiiip.ui.activity.template.HomeActivityTemplate
 
 class HomeActivity : PresentationActivity<HomeActivityPresenter>() {
 
+
     override fun createPresenter(): HomeActivityPresenter = HomeActivityPresenter(this, HomeActivityTemplate(this))
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/template/HomeActivityTemplate.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/template/HomeActivityTemplate.kt
@@ -1,10 +1,11 @@
 package com.github.chuross.qiiip.ui.activity.template
 
 import android.content.Context
+import android.support.v4.widget.DrawerLayout
 import com.github.chuross.chuross.qiiip.R
 import com.github.chuross.library.mvp.view.template.NonApplyTemplate
 
 class HomeActivityTemplate(context: Context) : NonApplyTemplate(context, R.layout.template_activity_home) {
 
-
+    val drawerLayout: DrawerLayout by lazy { view.findViewById(R.id.layout_drawer) as DrawerLayout }
 }
