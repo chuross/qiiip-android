465de41a970878a42b2eff2867ef3962ba988b73
chuross <azarasi.mo.mo@gmail.com>
2015-12-22 04:11:42 +0900

    詳細画面にタグを配置した

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/fragment/template/ItemDetailScreenFragmentTemplate.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/fragment/template/ItemDetailScreenFragmentTemplate.kt
@@ -5,6 +5,7 @@ import com.github.chuross.chuross.qiiip.R
 import com.github.chuross.library.mvp.view.template.NonApplyTemplate
 import com.github.chuross.library.mvp.view.template.SourceTemplate
 import com.github.chuross.qiiip.domain.item.Item
+import kotlinx.android.synthetic.template_fragment_item_detail_screen.view.tag_group
 import kotlinx.android.synthetic.template_fragment_item_detail_screen.view.toolbar
 import kotlinx.android.synthetic.template_fragment_item_detail_screen.view.txt_title
 
@@ -12,9 +13,14 @@ class ItemDetailScreenFragmentTemplate(context: Context) : NonApplyTemplate(cont
 
     val toolbar = view.toolbar
     val titleText = view.txt_title
+    val tagGroup = view.tag_group
 
     override fun apply(item: Item) {
         titleText.text = item.metaInfo?.title
+
+        item.metaInfo?.tags?.isNotEmpty().run {
+            tagGroup.setTags(item.metaInfo?.tags?.map { tag -> tag.getIdentity().getValue() })
+        }
     }
 
 }
