ca72c3909b4a859fa189d22e3c122612e21d254d
chuross <azarasi.mo.mo@gmail.com>
2015-12-22 01:07:58 +0900

    記事一覧のレイアウトを微修正

---
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/fragment/presenter/ItemDetailFragmentPresenter.kt
@@ -0,0 +1,9 @@
+package com.github.chuross.qiiip.ui.fragment.presenter
+
+import com.github.chuross.library.mvp.presenter.SupportFragmentPresenter
+import com.github.chuross.qiiip.ui.fragment.ItemDetailFragment
+import com.github.chuross.qiiip.ui.fragment.template.ItemDetailFragmentTemplate
+
+class ItemDetailFragmentPresenter(fragment: ItemDetailFragment) : SupportFragmentPresenter<ItemDetailFragment, ItemDetailFragmentTemplate>(fragment) {
+
+}
---
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/fragment/template/ItemDetailFragmentTemplate.kt
@@ -0,0 +1,9 @@
+package com.github.chuross.qiiip.ui.fragment.template
+
+import android.content.Context
+import com.github.chuross.chuross.qiiip.R
+import com.github.chuross.library.mvp.view.template.NonApplyTemplate
+
+class ItemDetailFragmentTemplate(context: Context) : NonApplyTemplate(context, R.layout.template_fragment_item_detail) {
+
+}
