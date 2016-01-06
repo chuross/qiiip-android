43de7ac1b11217b9cad334bd44bca6067a59925f
chuross <azarasi.mo.mo@gmail.com>
2016-01-07 03:08:46 +0900

    ログイン処理中にプログレスを表示するようにした

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/presenter/LoginActivityPresenter.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/presenter/LoginActivityPresenter.kt
@@ -6,18 +6,17 @@ import com.github.chuross.qiiip.application.Application
 import com.github.chuross.qiiip.domain.user.User
 import com.github.chuross.qiiip.infrastructure.qiita.QiitaV2Api
 import com.github.chuross.qiiip.infrastructure.qiita.request.TokenRequest
-import com.github.chuross.qiiip.ui.EmptyTemplate
 import com.github.chuross.qiiip.ui.activity.LoginActivity
-import org.apache.commons.lang3.exception.ExceptionUtils
+import com.github.chuross.qiiip.ui.activity.template.LoginActivityTemplate
 import rx.Observable
 import javax.inject.Inject
 
-class LoginActivityPresenter(activity: LoginActivity) : ActivityPresenter<LoginActivity, EmptyTemplate>(activity) {
+class LoginActivityPresenter(activity: LoginActivity) : ActivityPresenter<LoginActivity, LoginActivityTemplate>(activity) {
 
     @Inject
     lateinit var api: QiitaV2Api
 
-    override fun createTemplate(p0: LoginActivity?): EmptyTemplate = EmptyTemplate(view)
+    override fun createTemplate(p0: LoginActivity?): LoginActivityTemplate = LoginActivityTemplate(view)
 
     fun request(): Observable<User> {
         val application = Application.from(view)
@@ -42,13 +41,12 @@ class LoginActivityPresenter(activity: LoginActivity) : ActivityPresenter<LoginA
     }
 
     fun onRequestSuccess() {
-        //TODO show success message
-        Toast.makeText(view, "success!", Toast.LENGTH_LONG).show()
+        Toast.makeText(view, "ログインが完了しました", Toast.LENGTH_LONG).show()
         view.finish()
     }
 
     fun onRequestFailed(tr: Throwable) {
-        //TODO show success message
-        Toast.makeText(view, "failed!${ExceptionUtils.getStackTrace(tr)}", Toast.LENGTH_LONG).show()
+        Toast.makeText(view, "ログインに失敗しました", Toast.LENGTH_LONG).show()
+        view.finish()
     }
 }
---
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/activity/template/LoginActivityTemplate.kt
@@ -0,0 +1,9 @@
+package com.github.chuross.qiiip.ui.activity.template
+
+import android.content.Context
+import com.github.chuross.chuross.qiiip.R
+import com.github.chuross.library.mvp.view.template.AbstractTemplate
+
+class LoginActivityTemplate(context: Context) : AbstractTemplate(context, R.layout.template_activity_login) {
+
+}
