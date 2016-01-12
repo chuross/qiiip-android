01852da228482e9585b4c8ace45551c9be908c86
chuross <azarasi.mo.mo@gmail.com>
2016-01-13 03:17:47 +0900

    いらないクラスを削除した

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/fragment/presenter/EmptyFragmentPresenter.kt
+++
@@ -1,8 +0,0 @@
-package com.github.chuross.qiiip.ui.fragment.presenter
-
-import android.support.v4.app.Fragment
-import com.github.chuross.library.mvp.presenter.SupportFragmentPresenter
-
-abstract class EmptyFragmentPresenter<F : Fragment>(fragment: F) : SupportFragmentPresenter<F>(fragment) {
-
-}
