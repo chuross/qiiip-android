b4fc61144754ac126b8ca9a23fe57e455d08c84d
chuross <azarasi.mo.mo@gmail.com>
2017-03-23 01:48:54 +0900

    View拡張のパッケージを変更した

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/extension/ImageView.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/extension/ImageView.kt
@@ -1,11 +1,11 @@
-package com.github.chuross.qiiip.infrastructure.extension
+package com.github.chuross.qiiip.ui.extension
 
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
-@BindingAdapter("bind:imageUrl")
+@BindingAdapter("imageUrl")
 fun ImageView.loadImage(imageUrl: String) {
     Picasso.with(context).load(imageUrl).apply {
         fit()
