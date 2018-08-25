074e964dde9bde449c530b6d527f662e7cb6e87f
chuross <azarasi.mo.mo@gmail.com>
2018-08-26 01:04:29 +0900

    add ArcMotion

--- app/src/main/kotlin/com/github/chuross/qiiip/ui/transition/SimpleImageTransitionFactory.kt
+++ app/src/main/kotlin/com/github/chuross/qiiip/ui/transition/SimpleImageTransitionFactory.kt
@@ -1,9 +1,6 @@
 package com.github.chuross.qiiip.ui.transition
 
-import androidx.transition.ChangeBounds
-import androidx.transition.ChangeImageTransform
-import androidx.transition.ChangeTransform
-import androidx.transition.TransitionSet
+import androidx.transition.*
 import com.github.chuross.morirouter.core.TransitionFactory
 
 class SimpleImageTransitionFactory : TransitionFactory {
@@ -11,6 +8,7 @@ class SimpleImageTransitionFactory : TransitionFactory {
     override fun create(): Any {
         return TransitionSet().also {
             it.ordering = TransitionSet.ORDERING_TOGETHER
+            it.setPathMotion(ArcMotion())
             it.addTransition(ChangeBounds())
             it.addTransition(ChangeTransform())
             it.addTransition(ChangeImageTransform())
